d507f81ea42ab43c029d626d1f0bdf17c2b4daa0
alichohan56 <alihumamchohan@gmail.com>
2025-11-20 21:12:10 +0500

    up

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -226,6 +226,10 @@ class $AssetsImagesGen {
   AssetGenImage get cancelicon =>
       const AssetGenImage('assets/images/cancelicon.png');
 
+  /// File path: assets/images/cardimage.png
+  AssetGenImage get cardimage =>
+      const AssetGenImage('assets/images/cardimage.png');
+
   /// File path: assets/images/course_complete.png
   AssetGenImage get courseComplete =>
       const AssetGenImage('assets/images/course_complete.png');
@@ -292,6 +296,30 @@ class $AssetsImagesGen {
   /// File path: assets/images/logo2.png
   AssetGenImage get logo2 => const AssetGenImage('assets/images/logo2.png');
 
+  /// File path: assets/images/menuicon1.png
+  AssetGenImage get menuicon1 =>
+      const AssetGenImage('assets/images/menuicon1.png');
+
+  /// File path: assets/images/menuicon2.png
+  AssetGenImage get menuicon2 =>
+      const AssetGenImage('assets/images/menuicon2.png');
+
+  /// File path: assets/images/menuicon3.png
+  AssetGenImage get menuicon3 =>
+      const AssetGenImage('assets/images/menuicon3.png');
+
+  /// File path: assets/images/menuicon4.png
+  AssetGenImage get menuicon4 =>
+      const AssetGenImage('assets/images/menuicon4.png');
+
+  /// File path: assets/images/menuicon5.png
+  AssetGenImage get menuicon5 =>
+      const AssetGenImage('assets/images/menuicon5.png');
+
+  /// File path: assets/images/menuicon6.png
+  AssetGenImage get menuicon6 =>
+      const AssetGenImage('assets/images/menuicon6.png');
+
   /// File path: assets/images/notificationicon.png
   AssetGenImage get notificationicon =>
       const AssetGenImage('assets/images/notificationicon.png');
@@ -350,6 +378,10 @@ class $AssetsImagesGen {
   AssetGenImage get signInoption =>
       const AssetGenImage('assets/images/signInoption.png');
 
+  /// File path: assets/images/signouticon.png
+  AssetGenImage get signouticon =>
+      const AssetGenImage('assets/images/signouticon.png');
+
   /// File path: assets/images/slider.png
   AssetGenImage get slider => const AssetGenImage('assets/images/slider.png');
 
@@ -377,6 +409,7 @@ class $AssetsImagesGen {
     bannerSvg,
     bellringicon,
     cancelicon,
+    cardimage,
     courseComplete,
     createpage,
     envelopeicon,
@@ -394,6 +427,12 @@ class $AssetsImagesGen {
     lessonComplete,
     likeicon,
     logo2,
+    menuicon1,
+    menuicon2,
+    menuicon3,
+    menuicon4,
+    menuicon5,
+    menuicon6,
     notificationicon,
     notificationsicon,
     onboard,
@@ -409,6 +448,7 @@ class $AssetsImagesGen {
     sentence,
     settingsicon,
     signInoption,
+    signouticon,
     slider,
     subscriptionBg,
     watchtimeicon,
--- lib/shared/widgets/cardwidget/card_widget.dart
+++ lib/shared/widgets/cardwidget/card_widget.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import '../../../gen/assets.gen.dart';
 
 class CardWidget extends StatefulWidget {
-  const CardWidget({super.key});
+  final String backgroundImage;
+  const CardWidget({super.key, required this.backgroundImage});
 
   @override
   State<CardWidget> createState() => _CardWidgetState();
@@ -16,25 +17,18 @@ class _CardWidgetState extends State<CardWidget> {
       width: 125,
       height: 180,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10), 
+        borderRadius: BorderRadius.circular(10),
         image: DecorationImage(
-          image: NetworkImage(
-            'https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D',
-          ),
+          image: NetworkImage(widget.backgroundImage),
           fit: BoxFit.cover,
         ),
       ),
       child: Padding(
-        padding:  EdgeInsets.all(8),
+        padding: EdgeInsets.all(8),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Container(),
-            Image.asset(
-              Assets.images.envelopeicon.path
-            )
-          ],
+          children: [Image.asset(Assets.images.envelopeicon.path, width: 20)],
         ),
       ),
     );
