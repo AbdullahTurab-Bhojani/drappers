710b1d149c2064f6689a28844df83f8b902a914d
Muhammad Haadi Chohan <52502353+haady13@users.noreply.github.com>
2025-12-08 14:40:29 +0500

    applyToPitchSCroLL

--- lib/features/applypitch/presentation/apply_pitch_screen.dart
+++ lib/features/applypitch/presentation/apply_pitch_screen.dart
@@ -56,146 +56,142 @@ class _ApplyPitchState extends State<ApplyPitch> {
               elevation: 0,
             ),
 
-            Expanded(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 30),
-                      PoppinsText(
-                        "Submit Your Pitch",
-                        fontSize: PoppinsFontSizeVariant.size16,
-                        fontWeight: PoppinsFontWeightVariant.medium,
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 20),
+                  PoppinsText(
+                    "Submit Your Pitch",
+                    fontSize: PoppinsFontSizeVariant.size16,
+                    fontWeight: PoppinsFontWeightVariant.medium,
+                  ),
+                  SizedBox(height: 18),
+                  PoppinsText(
+                    "Apply to appeal on Meet the Drapers and pitch your startup to investors.",
+                    fontSize: PoppinsFontSizeVariant.size12,
+                    fontWeight: PoppinsFontWeightVariant.regular,
+                    height: 1.7,
+                  ),
+                  SizedBox(height: 16),
+            
+                  NewTextField(
+                    fieldbg: AppColors.tfield,
+                    controller: _fullNameController,
+                    labelText: "Full Name*",
+                    hintText: "johnmackson",
+                    hintStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: customColors.textColor),
+                  ),
+                  SizedBox(height: 16),
+            
+                  NewTextField(
+                    fieldbg: AppColors.tfield,
+                    controller: _startupController,
+                    labelText: "Startup Name*",
+                    hintText: "Krypto",
+                    hintStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: customColors.textColor),
+                  ),
+                  SizedBox(height: 16),
+                  NewTextField(
+                    fieldbg: AppColors.tfield,
+                    controller: _emailController,
+                    labelText: "Email Address*",
+                    hintText: "Enter your email",
+                    hintStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: customColors.textColor),
+                  ),
+                  SizedBox(height: 16),
+                  NewTextField(
+                    fieldbg: AppColors.tfield,
+                    controller: _linkController,
+                    labelText: "Shareable link for your pitch deck*",
+                    hintText: "www.link.com",
+                    hintStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: customColors.textColor),
+                  ),
+                  SizedBox(height: 16),
+                  PoppinsText(
+                    "Share a link to your pitch deck (Google Drive, DocSend, Dropbox, etc.)",
+                    fontSize: PoppinsFontSizeVariant.size12,
+                    fontWeight: PoppinsFontWeightVariant.regular,
+                    height: 1.7,
+                  ),
+                  SizedBox(height: 16),
+            
+                  DropDownField(
+                    controller: _locationController,
+                    labelText: "Location*",
+                    hintText: "Select your location",
+                    hintStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: customColors.textColor),
+                    // validator: (value) {
+                    //   if (value == null || value.trim().isEmpty) {
+                    //     return "Full name required";
+                    //   }
+                    //   return null;
+                    // },
+                    sufixIcon: Icon(
+                      Icons.keyboard_arrow_down,
+                      color: Colors.white,
+                      size: 24,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+            
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: customColors.dark,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
                       ),
-                      SizedBox(height: 18),
-                      PoppinsText(
-                        "Apply to appeal on Meet the Drapers and pitch your startup to investors.",
-                        fontSize: PoppinsFontSizeVariant.size12,
-                        fontWeight: PoppinsFontWeightVariant.regular,
-                        height: 1.7,
-                      ),
-                      SizedBox(height: 16),
-
-                      NewTextField(
-                        fieldbg: AppColors.tfield,
-                        controller: _fullNameController,
-                        labelText: "Full Name*",
-                        hintText: "johnmackson",
-                        hintStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: customColors.textColor),
-                      ),
-                      SizedBox(height: 16),
-
-                      NewTextField(
-                        fieldbg: AppColors.tfield,
-                        controller: _startupController,
-                        labelText: "Startup Name*",
-                        hintText: "Krypto",
-                        hintStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: customColors.textColor),
-                      ),
-                      SizedBox(height: 16),
-                      NewTextField(
-                        fieldbg: AppColors.tfield,
-                        controller: _emailController,
-                        labelText: "Email Address*",
-                        hintText: "Enter your email",
-                        hintStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: customColors.textColor),
-                      ),
-                      SizedBox(height: 16),
-                      NewTextField(
-                        fieldbg: AppColors.tfield,
-                        controller: _linkController,
-                        labelText: "Shareable link for your pitch deck*",
-                        hintText: "www.link.com",
-                        hintStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: customColors.textColor),
-                      ),
-                      SizedBox(height: 16),
-                      PoppinsText(
-                        "Share a link to your pitch deck (Google Drive, DocSend, Dropbox, etc.)",
-                        fontSize: PoppinsFontSizeVariant.size12,
-                        fontWeight: PoppinsFontWeightVariant.regular,
-                        height: 1.7,
-                      ),
-                      SizedBox(height: 16),
-
-                      DropDownField(
-                        controller: _locationController,
-                        labelText: "Location*",
-                        hintText: "Select your location",
-                        hintStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: customColors.textColor),
-                        // validator: (value) {
-                        //   if (value == null || value.trim().isEmpty) {
-                        //     return "Full name required";
-                        //   }
-                        //   return null;
-                        // },
-                        sufixIcon: Icon(
-                          Icons.keyboard_arrow_down,
-                          color: Colors.white,
-                          size: 24,
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          Assets.images.infoicon.path,
+                          width: 16,
+                          height: 16,
                         ),
-                      ),
-                      SizedBox(height: 16),
-
-                      Container(
-                        width: double.infinity,
-                        padding: EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          color: customColors.dark,
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(10),
-                            bottomLeft: Radius.circular(10),
-                            bottomRight: Radius.circular(10),
+                        SizedBox(width: 8),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              PoppinsText(
+                                "What happens next?",
+                                fontSize: PoppinsFontSizeVariant.size12,
+                                fontWeight:
+                                    PoppinsFontWeightVariant.semiBold,
+                              ),
+                              SizedBox(height: 6),
+                              PoppinsText(
+                                "Our team will review your application and pitchdeck. If selected, you’ll be contacted within 2-3 weeks to schedule your appearance on Meet the Drapers.",
+                                fontSize: PoppinsFontSizeVariant.size12,
+                                fontWeight:
+                                    PoppinsFontWeightVariant.regular,
+                                maxLines: 5,
+                              ),
+                            ],
                           ),
                         ),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Image.asset(
-                              Assets.images.infoicon.path,
-                              width: 16,
-                              height: 16,
-                            ),
-                            SizedBox(width: 8),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  PoppinsText(
-                                    "What happens next?",
-                                    fontSize: PoppinsFontSizeVariant.size12,
-                                    fontWeight:
-                                        PoppinsFontWeightVariant.semiBold,
-                                  ),
-                                  SizedBox(height: 6),
-                                  PoppinsText(
-                                    "Our team will review your application and pitchdeck. If selected, you’ll be contacted within 2-3 weeks to schedule your appearance on Meet the Drapers.",
-                                    fontSize: PoppinsFontSizeVariant.size12,
-                                    fontWeight:
-                                        PoppinsFontWeightVariant.regular,
-                                    maxLines: 5,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: 30),
-                      Container(
-                        height: 50, width: 390,
-                        child: AppButton(
-                          onPressed: () {
-                            context.goNamed(AppRoutes.home.name);
-                          },
-                          title: 'Submit',
-                        ),
-                      ),
-                      SizedBox(height: 100),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
+                  SizedBox(height: 30),
+                  Container(
+                    height: 50, width: 390,
+                    child: AppButton(
+                      onPressed: () {
+                        context.goNamed(AppRoutes.home.name);
+                      },
+                      title: 'Submit',
+                    ),
+                  ),
+                  SizedBox(height: 80),
+                ],
               ),
             ),
           ],
