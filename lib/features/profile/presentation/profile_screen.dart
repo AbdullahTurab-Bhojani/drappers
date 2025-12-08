6aa94d8213ca730c9066f3153c4c944b956e8165
Muhammad Haadi Chohan <52502353+haady13@users.noreply.github.com>
2025-12-08 16:03:35 +0500

    imageProfile

--- lib/features/profile/presentation/profile_screen.dart
+++ lib/features/profile/presentation/profile_screen.dart
@@ -109,12 +109,15 @@ class ProfileScreen extends StatelessWidget {
                 child: SingleChildScrollView(
                   child:   Column(
                   children: [
-                    CircleAvatar(
-                      radius: 60,
-                      // radius: isSelected ? 15 : 15,
-                      backgroundColor: Colors.transparent,
-                      backgroundImage: NetworkImage(
-                        'https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D',
+                    Container(
+                      height: 114.82, width: 114.82,
+                      child: CircleAvatar(
+                        radius: 60,
+                        // radius: isSelected ? 15 : 15,
+                        backgroundColor: Colors.transparent,
+                        backgroundImage: NetworkImage(
+                          'https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D',
+                        ),
                       ),
                     ),
                     SizedBox(height: 20),
