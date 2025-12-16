b23ca6f2eb42293c6b0e63b478c77ef2d4010cbd
alichohan56 <alihumamchohan@gmail.com>
2025-12-17 00:02:34 +0500

    up

--- lib/features/splash/presentation/views/splash.dart
+++ lib/features/splash/presentation/views/splash.dart
@@ -4,9 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:video_player/video_player.dart';
-
 import '../../../../drappers.dart';
-import '../../../../gen/assets.gen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -49,25 +47,18 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _controller.value.isInitialized
-          ? SizedBox.expand(
-              child: FittedBox(
-                fit: BoxFit.cover,
-                child: SizedBox(
-                  width: _controller.value.size.width,
-                  height: _controller.value.size.height,
-                  child: VideoPlayer(_controller),
-                ),
-              ),
-            )
-          : Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(Assets.images.nativeSplash.path),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
+      body: SizedBox(
+        width: double.infinity, 
+        height: double.infinity, 
+        child: FittedBox(
+          fit: BoxFit.cover,
+          child: SizedBox(
+            width: _controller.value.size.width,
+            height: _controller.value.size.height,
+            child: VideoPlayer(_controller),
+          ),
+        ),
+      ),
     );
   }
 }
