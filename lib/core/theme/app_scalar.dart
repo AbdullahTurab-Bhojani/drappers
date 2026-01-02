fb24853e2db98ab00b4dde6fe7d1589ff9f6b31e
Abdullah Turab Bhojani <abdullah.bhojani@neksoft.com>
2026-01-02 16:39:46 +0500

    up

--- lib/core/theme/app_scalar.dart
+++ lib/core/theme/app_scalar.dart
@@ -4,13 +4,27 @@ class AppScaler {
   static const double referenceWidth = 440;
   static const double referenceHeight = 956;
 
+  static bool _isLandscape(BuildContext context) {
+    return MediaQuery.of(context).orientation == Orientation.landscape;
+  }
+
   static double scaleSize(BuildContext context, double size) {
-    final width = MediaQuery.of(context).size.width;
-    return size * (width / referenceWidth);
+    final media = MediaQuery.of(context).size;
+
+    final screenWidth = _isLandscape(context) ? media.height : media.width;
+
+    final baseWidth = _isLandscape(context) ? referenceHeight : referenceWidth;
+
+    return size * (screenWidth / baseWidth);
   }
 
   static double scaleHeight(BuildContext context, double size) {
-    final height = MediaQuery.of(context).size.height;
-    return size * (height / referenceHeight);
+    final media = MediaQuery.of(context).size;
+
+    final screenHeight = _isLandscape(context) ? media.width : media.height;
+
+    final baseHeight = _isLandscape(context) ? referenceWidth : referenceHeight;
+
+    return size * (screenHeight / baseHeight);
   }
 }
